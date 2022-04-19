0376847506999085d39a9dafff82cf468fa8480f
abedshamia <ab.shamia96@gmail.com>
2022-04-20 02:45:30 +0300

    Add More Styling to the app

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -1,17 +1,16 @@
-// import React , {useState , useEffect} from 'react'
-//  import Card from './Card'
-// export default function Cards() {
- 
-//   return (
-//     <div className='container'>
-//       <div className="cards">
-//        {/* {games.map((game) =>{
-//             return (
-//             <Card />
-//             ) 
-//         })} */}
-//         <Card />
-//  </div>
-//     </div>
-//   )
-// }
+import React, {useState, useEffect} from 'react';
+import Card from './Card';
+export default function Cards() {
+  return (
+    <div className="container">
+      <div className="cards">
+        {/* {games.map((game) =>{
+            return (
+            <Card />
+            ) 
+        })} */}
+        <Card />
+      </div>
+    </div>
+  );
+}
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,3 +1,19 @@
-// const searchHandle = (e) =>{
-//   setSearch(e.target.value)
-// }
+import React from 'react';
+import './Search.css';
+function Search() {
+  return (
+    <div>
+      <form className="form-control">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search for a deal..."
+          className="input-control"
+        />
+        <button className="btn">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
