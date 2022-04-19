99ac6da00a89db97a554bf5f625628bd6bd306cd
Maram Ismail <maramismail2015@gmail.com>
2022-04-20 02:01:04 +0300

    fic

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+// export default function Card() {
+//   return (
+//     <div className='card'>
+//      <div className="card-img">
+//        <img src="" alt="" />
+//      </div>
+
+//      <div className="card-body">
+//        <h2 className='game-title'> batman </h2>
+//        <span className='sale-price'>10</span>
+//        <span className='normal-price'>50</span>
+//        <span className='game-rating'> 84 % </span>
+//      </div>
+//     </div>
+//   )
+// }
---
+++ src/Components/Card/Cards.jsx
@@ -0,0 +1,17 @@
+// import React , {useState , useEffect} from 'react'
+//  import Card from './Card'
+// export default function Cards() {
+ 
+//   return (
+//     <div className='container'>
+//       <div className="cards">
+//        {/* {games.map((game) =>{
+//             return (
+//             <Card />
+//             ) 
+//         })} */}
+//         <Card />
+//  </div>
+//     </div>
+//   )
+// }
